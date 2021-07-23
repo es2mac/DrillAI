472f4819aaae38055fa263e6d03a7c07adf533fd
Hsing Liu <liu.hsing@gmail.com>
2021-07-23 16:15:51 +0800

    Simplify Piece coding

--- Sources/DrillAI/Tetris/Piece.swift
+++ Sources/DrillAI/Tetris/Piece.swift
@@ -25,22 +25,22 @@ public struct Piece {
 
 
 extension Piece: Hashable {
-    // This encoding is unique and reversible, so we can store the whole piece compactly as jsut an Int.
+    /// This encoding is unique and reversible (for reasonable height y),
+    /// so we can store the whole piece compactly as just an Int.
     var code: Int {
         return ((x + y * 10) << 5) | (type.rawValue << 2) | orientation.rawValue
     }
     
-    init?(code: Int) {
-        // Hash of 0 would be flat I at (0, 0) which in fact doesn't fit on board, so can ignore
-        guard code != 0 else { return nil }
+    init(code: Int) {
         let x = (code >> 5) % 10
         let y = (code >> 5) / 10
-        // Assume center should be in the 10x20 field, though there could be edge
-        // cases in the real world where y goes >= 20
-        guard 0..<10 ~= x, 0..<20 ~= y else { return nil }
         let type = Tetromino(rawValue: (code >> 2) & 0b111)!
         let orientation = Orientation(rawValue: code & 0b11)!
         self.init(type: type, x: x, y: y, orientation: orientation)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(code)
     }
 }
 
