6a04a656bd47b85c1b664142be70080d3df71bd5
Hsing Liu <liu.hsing@gmail.com>
2021-08-01 23:02:13 +0800

    Simplify concurrency: Don't keep info, just 2 tasks

--- Sources/DrillAI/GeneratorBot.swift
+++ Sources/DrillAI/GeneratorBot.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 
+private let batchSize = 32
+
+
 public final class GeneratorBot<Evaluator: MCTSEvaluator> where Evaluator.State == GameState {
     public typealias State = GameState
     public typealias Action = Piece
     public typealias ActionVisits = MCTSTree<State>.ActionVisits
-    typealias EvaluationInfo = [(id: ObjectIdentifier, state: GameState, nextActions: [GameState.Action])]
-    typealias EvaluationResults = MCTSTree<GameState>.EvaluationResults
+    typealias EvaluationInfo = [(id: ObjectIdentifier, state: GameState, nextActions: [Action])]
 
     private let tree: MCTSTree<State>
     private let evaluator: Evaluator
@@ -22,107 +24,78 @@ public final class GeneratorBot<Evaluator: MCTSEvaluator> where Evaluator.State 
     private var thinkingTask: Task<Void, Never>?
     private var treeTask: Task<EvaluationInfo, Never>?
     private var evaluatorTask: Task<Void, Never>?
-    private var queuedInfo: EvaluationInfo? = nil
 
     public init(initialState: State, evaluator: Evaluator) {
         self.tree = MCTSTree(initialState: initialState)
         self.evaluator = evaluator
     }
+}
 
-    /// On the tree side: try to keep one EvaluationInfo queued for evaluation
-    /// On the evaluator sitde: if have queued EvaluationInfo, take it
-    public func startThinking() {
+
+public extension GeneratorBot {
+    func startThinking() {
         guard thinkingTask == nil else { return }
-        // spin off first tree task
-        addTreeTask()
-        // spin off thinking loop
         thinkingTask = Task {
             await coordinateThinking()
         }
     }
 
-    public func stopThinking() {
+    func stopThinking() {
         thinkingTask?.cancel()
         treeTask?.cancel()
         evaluatorTask?.cancel()
         thinkingTask = nil
         treeTask = nil
         evaluatorTask = nil
-        queuedInfo = nil
     }
 
-    public func getActions() async -> [ActionVisits] {
+    func getActions() async -> [ActionVisits] {
         return await tree.getOrderedRootActions()
     }
 
-    public func advance(with action: Action) async -> State {
+    func advance(with action: Action) async -> State {
         stopThinking()
         let newState = await tree.promoteRoot(action: action)
         return newState
     }
+}
 
-    private func addTreeTask() {
-        treeTask = Task {
-//            print("Tree picking")
-            let info = await tree.getNextUnevaluatedStates(targetCount: 32)
-//            print("Tree done picking")
-            return info
-        }
-    }
 
-    private func addEvaluatorTask(info: EvaluationInfo) {
-        evaluatorTask = Task {
-//            print("Start evaluation")
-            let results = await evaluator.evaluate(info: info)
-//            print("End evaluation")
-            if !Task.isCancelled {
-                await tree.updateWithEvaluationResults(results)
-            }
-//            print("Tree updated")
-        }
-    }
-
-    private func coordinateThinking() async {
+private extension GeneratorBot {
+    func coordinateThinking() async {
         while !Task.isCancelled {
-            switch (treeTask, queuedInfo, evaluatorTask) {
-            case (nil, nil, _):
-//                print("Case 1: Tree is idle, add tree task")
-                // Tree is idle & nothing queued for eval
+            switch (treeTask, evaluatorTask) {
+            case (nil, _):
+                // Tree is idle & nothing waiting for eval
                 addTreeTask()
 
-            case (_, .some(let info), nil):
-//                print("Case 2: Evaluator is idle, add evaluator task")
-                // Evaluator is idle
-                addEvaluatorTask(info: info)
-                queuedInfo = nil
+            case (.some(let treeTask), nil):
+                // Evaluator is idle, tree working or maybe done waiting with stuff to evaluate
+                let info = await treeTask.value
+                if !Task.isCancelled {
+                    addEvaluatorTask(info: info)
+                }
+                self.treeTask = nil
 
-            case (_, .some(_), .some(let evaluatorTask)):
-//                print("Case 3: Has tree result, wait on evaluator")
-                // Has tree result, but waiting on evaluator
+            case (.some(_), .some(let evaluatorTask)):
+                // Both working away, need the evaluator checked off first to offload tree results
                 await evaluatorTask.value
                 self.evaluatorTask = nil
+            }
+        }
+    }
 
-            case (.some(let treeTask), nil, nil):
-//                print("Case 4: Evaluator nothing to do, wait on tree")
-                // Tree busy, evaluator idle but nothing to do
-                let info = await treeTask.value
-                if !Task.isCancelled {
-                    queuedInfo = info
-                }
-                self.treeTask = nil
+    func addTreeTask() {
+        treeTask = Task {
+            await tree.getNextUnevaluatedStates(targetCount: batchSize)
+        }
+    }
 
-            case (.some(let treeTask), nil, .some(_)):
-//                print("Case 5: Both busy, choose to wait for tree")
-                // Both busy.  Here I have a choice of which one to wait on
-                // (because I don't know an easy way to wait for either).
-                // But in fact, since tree is an actor, and evaluation task
-                // includes updating the tree at the end, the eval task
-                // actually also waits on the tree task.
-                let info = await treeTask.value
-                if !Task.isCancelled {
-                    queuedInfo = info
-                }
-                self.treeTask = nil
+    func addEvaluatorTask(info: EvaluationInfo) {
+        evaluatorTask = Task {
+            let results = await evaluator.evaluate(info: info)
+            if !Task.isCancelled {
+                await tree.updateWithEvaluationResults(results)
             }
         }
     }
