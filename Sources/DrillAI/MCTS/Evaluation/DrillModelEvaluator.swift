052ef257b9fcfc9f0e244d5bee820e46db86b5ce
Hsing Liu <liu.hsing@gmail.com>
2021-08-27 17:40:37 +0800

    Encode model input and and decode model output, make it work

--- Sources/DrillAI/MCTS/Evaluation/DrillModelEvaluator.swift
+++ Sources/DrillAI/MCTS/Evaluation/DrillModelEvaluator.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 import CoreML
+import Accelerate
 
 
 public final class DrillModelEvaluator {
 
     let model: DrillModelCoreML
 
-    init(modelURL url: URL?) throws {
+    public init(modelURL url: URL? = nil) throws {
         if let url = url {
             self.model = try DrillModelCoreML(contentsOf: url)
         } else {
@@ -27,13 +28,16 @@ extension DrillModelEvaluator: MCTSEvaluator {
     public typealias State = GameState
 
     public func evaluate(info: MCTSTree<State>.StatesInfo) async -> MCTSTree.EvaluationResults {
-        var results: MCTSTree.EvaluationResults = []
+        let inputs = info.map(\.state).map(encode(state:)).map(DrillModelCoreMLInput.init)
+        let outputs = try! model.predictions(inputs: inputs)
 
-        for (id, state, nextActions) in info {
-            results.append((id: id, value: Double(state.remainingGarbageCount), priors: [Double](repeating: 0, count: nextActions.count)))
+        return zip(info, outputs).map { item, output in
+            (id: item.id,
+             value: Double(output.var_205[0]),
+             priors: calculatePriors(logits: output.var_186,
+                                     playPieceType: item.state.playPieceType,
+                                     actions: item.nextActions))
         }
-
-        return results
     }
 }
 
@@ -41,5 +45,60 @@ extension DrillModelEvaluator {
     func evaluate(_ shapedArray: MLShapedArray<Float32>) -> DrillModelCoreMLOutput? {
         let x = try? model.prediction(input: shapedArray)
         return x
+    }
+}
+
+
+extension DrillModelEvaluator {
+    func encode(state: GameState) -> MLShapedArray<Float> {
+        var shapedArray = MLShapedArray<Float>(repeating: 0, shape: [1, 43, 20, 10])
+
+        // Field
+        let field = state.field
+        let fieldCells: [Float] = field.storage.flatMap { row in
+            (0 ..< 10).map { i in
+                row & (1 << i) == 0 ? 0 : 1
+            }
+        }
+        shapedArray[0][0][0..<field.height].fill(with: fieldCells)
+
+        // Play pieces
+        let playPieceType = state.playPieceType
+        let tetrominos = [playPieceType] +
+                          (state.hold.map { [$0] } ?? []) +
+                          state.nextPieceTypes
+
+        for (index, type) in tetrominos.enumerated().prefix(6) {
+            shapedArray[0][1 + index * 7 + type.rawValue].fill(with: 1)
+        }
+
+        return shapedArray
+    }
+
+    func calculatePriors(logits: MLMultiArray,
+                         playPieceType: Tetromino,
+                         actions: [Piece]) -> [Double] {
+        // Logits are shaped (1, 8, 20, 10)
+        // First select the logit values
+        var values: [Float] = actions.map { piece in
+            let playPieceOffset = piece.type == playPieceType ? 0 : 800
+            let orientationOffset = 200 * piece.orientation.rawValue
+            return logits[playPieceOffset + orientationOffset + 10 * piece.y + piece.x].floatValue
+        }
+
+        // Softmax.  Reference hollance/CoreMLHelpers/CoreMLHelpers/Math.swift
+        let maximum = vDSP.maximum(values)
+        vDSP.add(-maximum, values, result: &values)
+
+        // Ad-hoc tweak:  Prior values are too extreme, smooth them out
+        let minimum = vDSP.minimum(values)
+        vDSP.divide(values, -minimum, result: &values)
+
+        // The rest is the usual
+        vForce.exp(values, result: &values)
+        let valueSum = vDSP.sum(values)
+        vDSP.divide(values, valueSum, result: &values)
+
+        return values.map(Double.init)
     }
 }
--- Tests/DrillAITests/DrillModelEvaluatorTests.swift
+++ Tests/DrillAITests/DrillModelEvaluatorTests.swift
@@ -17,7 +17,7 @@ final class DrillModelEvaluatorTests: XCTestCase {
         XCTAssertNoThrow(_ = try DrillModelEvaluator(modelURL: url))
     }
 
-    func testEval() throws {
+    func testTest() async throws {
         let bundle = Bundle(for: DrillModelCoreML.self)
         let url = bundle.url(forResource: "DrillModelCoreML",
                              withExtension: "mlmodelc",
@@ -26,11 +26,26 @@ final class DrillModelEvaluatorTests: XCTestCase {
 
         let evaluator = try DrillModelEvaluator(modelURL: url)
 
-        let shapedArray = MLShapedArray<Float>(repeating: 0, shape: [1, 43, 20, 10])
-        print(shapedArray)
-//        let result = evaluator.evaluate(shapedArray)!
-//        print(result.var_186ShapedArray)
-//        print(result.var_205ShapedArray)
+        // Borrowed from GameStateTests
+        // Piece seed 1 => [Z, L, S, J, T, I, O, O, J, T, Z, I, S, L]
+        var state = GameState(garbageCount: 100, garbageSeed: 1, pieceSeed: 1)
+        var piece = Piece(type: .L, x: 8, y: 8, orientation: .down)
+        state = state.getNextState(for: piece)
+
+        piece = Piece(type: .S, x: 6, y: 8, orientation: .up)
+        state = state.getNextState(for: piece)
+
+        piece = Piece(type: .J, x: 1, y: 8, orientation: .up)
+        state = state.getNextState(for: piece)
+
+//        print(state.field)
+
+//        let shapedArray = evaluator.encode(state: state)
+//        let output = evaluator.evaluate(shapedArray)!
+//        print(output.var_186ShapedArray)
+//        print(output.var_205ShapedArray)
+        let evaluations = await evaluator.evaluate(info: [(id: .init(self), state: state, nextActions: state.getLegalActions())])
+
     }
 }
 
